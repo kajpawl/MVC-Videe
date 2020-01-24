c961a1943368479156f1acb47739f03faeeb8d8a
Kajetan Pawliszyn <kpawliszyn@future-processing.com>
2020-01-24 16:11:27 +0100

    Add basic routing

--- Videe/Controllers/MoviesController.cs
+++ Videe/Controllers/MoviesController.cs
@@ -16,5 +16,27 @@ namespace Videe.Controllers
 
             return View(movie);
         }
+        
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
     }
 }
