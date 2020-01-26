b62723b2d682058e571107b10177118cbee9ae3d
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-01-26 13:50:12 +0100

    Add data annotations for Customer model, add migrations for membership type

--- Videe/Models/Customer.cs
+++ Videe/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,11 @@ namespace Videe.Models
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(235)]
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershiTypeId { get; set; }
     }
 }
