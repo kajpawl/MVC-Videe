f5cc67e223b77ae2f02b393425045f2fea5eccd4
Kajetan Pawliszyn <kpawliszyn@future-processing.com>
2020-01-27 12:06:48 +0100

    Add data annotation to Add New Customer's birthdate

--- Videe/Models/Customer.cs
+++ Videe/Models/Customer.cs
@@ -16,6 +16,8 @@ namespace Videe.Models
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
+
+        [Display(Name = "Date of Birth")]
         public DateTime? Birthdate { get; set; }
     }
 }
