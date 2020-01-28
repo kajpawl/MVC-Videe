9f8f7200668b3a15df9822d301fee280c07b83d4
Kajetan Pawliszyn <kpawliszyn@future-processing.com>
2020-01-28 16:21:39 +0100

    Add AntiForgeryToken

--- Videe/Controllers/CustomersController.cs
+++ Videe/Controllers/CustomersController.cs
@@ -35,6 +35,7 @@ namespace Videe.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
             if (!ModelState.IsValid)
