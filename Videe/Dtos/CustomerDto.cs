76d289a5e33a8e2d4510da7872fe7d604ee50e49
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-02-29 18:45:19 +0100

    Update CustomersController to fir REST API convention

--- Videe/Dtos/CustomerDto.cs
+++ Videe/Dtos/CustomerDto.cs
@@ -19,7 +19,7 @@ namespace Videe.Dtos
 
         public byte MembershipTypeId { get; set; }
 
-        [Min18YearsIfAMember]
+        // [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
 
     }
