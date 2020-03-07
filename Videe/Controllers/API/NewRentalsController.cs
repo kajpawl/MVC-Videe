e9acbb2a948a506f83b1946e05019a140938f881
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-03-07 18:02:25 +0100

    Add validation to NewRentals API

--- Videe/Controllers/API/NewRentalsController.cs
+++ Videe/Controllers/API/NewRentalsController.cs
@@ -21,14 +21,26 @@ namespace Videe.Controllers.API
         [HttpPost]
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
         {
-            var customer = _context.Customers.Single(
+            if (newRental.MovieIds.Count == 0)
+                return BadRequest("No movie Ids have been given");
+            
+            var customer = _context.Customers.SingleOrDefault(
                 s => s.Id == newRental.CustomerId);
 
+            if (customer == null)
+                return BadRequest("CustomerId is not valid");
+
             var movies = _context.Movies.Where(
-                m => newRental.MovieIds.Contains(m.Id));
+                m => newRental.MovieIds.Contains(m.Id)).ToList();
+
+            if (movies.Count != newRental.MovieIds.Count)
+                return BadRequest("One or more MovieIds are invalid.");
 
             foreach (var movie in movies)
             {
+                if (movie.NumberAvailable == 0)
+                    return BadRequest("Movie is not available");
+
                 movie.NumberAvailable--;
 
                 var rental = new Rental
