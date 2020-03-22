7cc2729e4735bcefbafebff27db292f6ae2942d8
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-03-22 14:16:10 +0100

    Move FB login data to Web.config, create deploy profile

--- Videe/App_Start/Startup.Auth.cs
+++ Videe/App_Start/Startup.Auth.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
@@ -55,8 +56,8 @@ namespace Videe
             //   consumerSecret: "");
 
             app.UseFacebookAuthentication(
-               appId: "3848954378463165",
-               appSecret: "1c5e0e38eea72bc6fdea190c95fbf16e");
+               appId: ConfigurationManager.AppSettings["FacebookAppId"],
+               appSecret: ConfigurationManager.AppSettings["FacebookAppSecret"]);
 
             //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             //{
